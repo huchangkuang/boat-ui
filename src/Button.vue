aa1a5843c108e9218004874095b05e1759d9d131
huchangkuang <603021137@qq.com>
2020-07-29 17:25:18 +0800

    项目初始化

---
+++ src/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button>按钮</button>
+</template>
+
+<script>
+  export default{
+    name: "Button"
+  }
+</script>
+
+<style lang="scss" scoped>
+button{
+    border: 1px solid red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from "vue"
+import Button from "./Button";
+
+Vue.component("b-button",Button)
+
+new Vue({
+  el: "#app"
+})
