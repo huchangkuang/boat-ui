c1d70cf0052240ca6030938f9472a0f551f734c2
huchangkuang <603021137@qq.com>
2020-07-30 21:23:38 +0800

    input组件支持input、change、blur、focus事件

--- src/Input.vue
+++ src/Input.vue
@@ -2,7 +2,9 @@
     <div class="wrapper" :class="{'error':error}">
         <input type="text" :value="value" :disabled="disabled" :readonly="readOnly"
                @change="$emit('change',$event.target.value)"
-               @input="$emit('input',$event.target.value)"/>
+               @input="$emit('input',$event.target.value)"
+               @focus="$emit('focus',$event.target.value)"
+               @blur="$emit('blur',$event.target.value)"/>
         <template v-if="error">
             <Icon icon="error" class="error"></Icon>
             <span class="error-info">{{error}}</span>
