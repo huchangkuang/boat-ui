ce721649bfc94f809934eaafe715bf745693e453
huchangkuang <603021137@qq.com>
2020-07-31 22:43:20 +0800

    完成栅格系统遗留的offset响应式，重构栅格系统代码

--- src/Col.vue
+++ src/Col.vue
@@ -17,6 +17,7 @@
     }
   export default {
     name: "Col",
+    offset:"0",
     props: {
       span: {
         type: [String, Number]
@@ -24,24 +25,34 @@
       offset: {
         type: [String, Number]
       },
-      ipad: {type: Object, validator},
-      narrowPc: {type: Object, validator},
+      ipad: {type: Object,validator},
+      narrowPc: {type: Object,validator},
       pc: {type: Object, validator},
-      widePc: {type: Object, validator}
+      widePc: {type: Object,validator}
     },
     data(){
       return {
         gutter:0
       }
     },
+    methods:{
+      createClass(obj,str=""){
+        if (!obj) {return []}
+        let classArray = []
+        if (obj.span) {classArray.push(`col-${str}${obj.span}`)}
+        if (obj.offset) {classArray.push(`offset-${str}${obj.offset}`)}
+        return classArray
+      }
+    },
     computed:{
       colClass(){
-        let {span,offset,ipad,narrowPc,pc,widePc} = this
-        return [span && `col-${span}`,offset && `offset-${offset}`,
-          ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-          ...(narrowPc ? [`col-narrowPc-${narrowPc.span}`] : []),
-          ...(pc ? [`col-pc-${pc.span}`] : []),
-          ...(widePc ? [`col-widePc-${widePc.span}`] : [])
+        let {span,offset,ipad,narrowPc,pc,widePc,createClass} = this
+        return [
+          ...createClass({span,offset}),
+          ...createClass(ipad,"ipad-"),
+          ...createClass(narrowPc,"narrowPc-"),
+          ...createClass(pc,"pc-"),
+          ...createClass(widePc,"widePc-")
         ]
       },
       colStyle(){
