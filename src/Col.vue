2fd3daba9251adaf8cb9a85e9d5ea15edf2d315b
huchangkuang <603021137@qq.com>
2020-07-31 11:31:26 +0800

    完成网格系统基本需求

---
+++ src/Col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="`col-${span}`"></div>
+</template>
+
+<script>
+  export default{
+    name: "Col",
+    props: {
+      span:{
+        type:[String,Number]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        width: 50%;
+        height: 100px;
+        border: 1px solid #333333;
+        $class-prefix: col-;
+        @for $n from 1 through 24{
+           &.#{$class-prefix}#{$n} {
+               width: $n/24*100%;
+           }
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Button from "./Button";
 import Icon from "./Icon";
 import ButtonGroup from "./ButtonGroup";
 import Input from "./Input";
+import Row from "./Row";
+import Col from "./Col";
+
 
 Vue.component("b-button",Button)
 Vue.component("Icon",Icon)
 Vue.component("b-button-group",ButtonGroup)
 Vue.component("b-input",Input)
+Vue.component("b-row",Row)
+Vue.component("b-col",Col)
 
 new Vue({
   el: "#app",
