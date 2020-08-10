5fc358a043602f15831a474fddaaa7bdb1b259a9
huchangkuang <603021137@qq.com>
2020-08-10 18:55:26 +0800

    实现toast组件防抖

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,23 @@
 import Toast from "./Toast";
 
+let currentToast
+function createToast({Vue,propsData,message}){
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData
+  })
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
+}
 export default {
   install(Vue,option){
     Vue.prototype.$toast = function (message,toastOption){
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOption
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue,propsData:toastOption,message})
     }
   }
 }
