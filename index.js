e1336ab2d4de67b5fa7d81f9373ca8f830ebbc78
huchangkuang <603021137@qq.com>
2020-08-12 20:10:19 +0800

    更新组件导出

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import Button from "./src/Button";
 import ButtonGroup from "./src/ButtonGroup";
-export {ButtonGroup,Button}
+import CollapseItem from "./src/CollapseItem";
+import Collapse from "./src/Collapse";
+import Popover from "./src/Popover";
+import Row from "./src/Row";
+import Col from "./src/Col";
+import Input from "./src/Input";
+import Layout from "./src/Layout";
+import Sider from "./src/Sider";
+import Content from "./src/Content";
+import Header from "./src/Header";
+import Footer from "./src/Footer";
+import Tabs from "./src/Tabs";
+import TabsHead from "./src/TabsHead";
+import TabsItem from "./src/TabsItem";
+import TabsBody from "./src/TabsBody";
+import TabsPane from "./src/TabsPane";
+import Toast from "./src/Toast";
+export {
+  ButtonGroup,Button,Collapse,CollapseItem,Popover,Row,Col,Input,Layout,Sider,
+  Content, Header,Footer,Tabs,TabsBody,TabsHead,TabsPane,TabsItem,Toast
+}
